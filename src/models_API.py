58d334ff36488543abb20bdf74dd51da4709b526
ericguizzo <eric.guizzo@hotmail.it>
2019-07-30 15:02:14 +0100

    refactoring API

--- src/models_API.py
+++ src/models_API.py
@@ -45,7 +45,7 @@ def EXAMPLE_model(time_dim, features_dim, model_parameters='0'):
     #THEN CALL THIS FUNCTION TO OVERWRITE DEFAULT PARAMETERS
     #WITH PARAMETERS DEFINED IN THE UI SCRIPT
     parse_parameters(model_parameters)
-    print (parameters)
+    print (model_parameters)
     sys.exit(0)
 
     #FINALLY DECLARE YOUR ARCHITECTURE AND RETURN THE MODEL
