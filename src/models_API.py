02e927952434bf850d8d1e379ce7d3480e51ce34
ericguizzo <eric.guizzo@hotmail.it>
2019-07-31 15:15:55 +0100

    refactoring API

--- src/models_API.py
+++ src/models_API.py
@@ -27,7 +27,7 @@ def parse_parameters(defaults, parameters):
 
 #DEFINE HERE YOUR MODELS!!
 
-def EXAMPLE_model(time_dim, features_dim, user_parameters='0'):
+def EXAMPLE_model(time_dim, features_dim, user_parameters='0=0'):
     '''
     to use this model, simply call architecture=EXAMPLE_model as a parameter
     in the UI script
