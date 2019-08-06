8e4a5c62e63840a75095a74e2ef86653051ccc07
ericguizzo <eric.guizzo@hotmail.it>
2019-08-06 15:11:18 +0100

    refactoring API.

--- src/training.py
+++ src/training.py
@@ -332,9 +332,9 @@ def main():
         train_score = best_model.evaluate_generator(training_generator, steps=len(training_target)/batch_size)
         val_score = best_model.evaluate_generator(validation_generator, steps=len(validation_target)/batch_size)
         test_score = best_model.evaluate_generator(test_generator, steps=len(test_target)/batch_size)
-        train_pred = best_model.predict_generator(training_generator, steps=len(training_target)/batch_size)
-        val_pred = best_model.predict_generator(validation_generator, steps=len(validation_target)/batch_size)
-        test_pred = best_model.predict_generator(test_generator, steps=len(test_target)/batch_size)
+        train_pred = best_model.predict_generator(training_generator, steps=1)
+        val_pred = best_model.predict_generator(validation_generator, steps=1)
+        test_pred = best_model.predict_generator(test_generator, steps=1)
     else:
         train_score = best_model.evaluate(training_predictors, training_target)
         val_score = best_model.evaluate(validation_predictors, validation_target)
