de75ae2e95024cc054a2b0596729040cf16617ef
ericguizzo <eric.guizzo@hotmail.it>
2019-08-07 12:17:37 +0100

    finishing code refactoring

--- src/training.py
+++ src/training.py
@@ -308,16 +308,10 @@ def main():
     if generator:  #if loading one batch at time to GPU
         datagen = ImageDataGenerator()
 
-        training_generator = datagen.flow(training_predictors, training_target, batch_size=batch_size,
-                                                shuffle=shuffle_training_data)
-        validation_generator = datagen.flow(validation_predictors, validation_target, batch_size=batch_size,
-                                            shuffle=False)
-        test_generator = datagen.flow(test_predictors, test_target, batch_size=batch_size,
-                                      shuffle=False)
-
-        history = locals()['model'].fit_generator(training_generator, validation_data=validation_generator,
-                                validation_steps=len(validation_target)/batch_size, callbacks=callbacks_list,
-                                steps_per_epoch=len(training_target)/batch_size, epochs=num_epochs, shuffle=shuffle_training_data)
+        history = locals()['model'].fit_generator(datagen.flow(training_predictors, training_target, batch_size=batch_size,
+                shuffle=shuffle_training_data), validation_data=datagen.flow(validation_predictors, validation_target, batch_size=batch_size,
+                shuffle=False), validation_steps=len(validation_target)/batch_size, callbacks=callbacks_list,
+                steps_per_epoch=len(training_target)/batch_size, epochs=num_epochs, shuffle=shuffle_training_data)
 
     else:  #if loading all dataset to GPU
         history = locals()['model'].fit(training_predictors,training_target, epochs=num_epochs,
@@ -343,10 +337,13 @@ def main():
         train_score = best_model.evaluate_generator(training_generator, steps=len(training_target)/batch_size)
         val_score = best_model.evaluate_generator(validation_generator, steps=len(validation_target)/batch_size)
         test_score = best_model.evaluate_generator(test_generator, steps=len(test_target)/batch_size)
-        train_pred = best_model.predict_generator(datagen.flow(training_predictors, training_target, batch_size=batch_size,
-                                                shuffle=False), steps=len(training_target)/batch_size)
-        val_pred = best_model.predict_generator(validation_generator, steps=len(validation_target)/batch_size)
-        test_pred = best_model.predict_generator(test_generator, steps=len(test_target)/batch_size)
+
+        train_pred = best_model.predict_generator(datagen.flow(training_predictors, training_target,
+                        batch_size=batch_size, shuffle=False), steps=len(training_target)/batch_size)
+        val_pred = best_model.predict_generator(datagen.flow(validation_predictors, validation_target,
+                        batch_size=batch_size, shuffle=False), steps=len(validation_target)/batch_size)
+        test_pred = best_model.predict_generator(datagen.flow(test_predictors, test_target,
+                        batch_size=batch_size, shuffle=False), steps=len(test_target)/batch_size)
     else:
         train_score = best_model.evaluate(training_predictors, training_target)
         val_score = best_model.evaluate(validation_predictors, validation_target)
