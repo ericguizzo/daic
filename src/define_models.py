2f4fcda313c34c26f67799490e0cb6fa8c77f7a9
ericguizzo <eric.guizzo@hotmail.it>
2019-08-12 12:31:11 +0100

    building new models

--- src/define_models.py
+++ src/define_models.py
@@ -2,10 +2,9 @@ from __future__ import print_function
 import numpy as np
 import configparser
 import matplotlib.pyplot as plt
-from keras.models import Model
+from keras.models import Model, Sequential
 from keras.layers import Input, Convolution2D, MaxPooling2D, Dense, Dropout, Activation, Flatten, Reshape
 from keras.layers.normalization import BatchNormalization
-from keras.layers.advanced_activations import ELU
 from keras.callbacks import EarlyStopping, ModelCheckpoint, History
 from keras.utils import np_utils
 from keras.backend import int_shape
