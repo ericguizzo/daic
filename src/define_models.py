48bb421f338d1f872ebb84485b751191a1b5fab0
ericguizzo <eric.guizzo@hotmail.it>
2019-08-12 12:32:55 +0100

    building new models

--- src/define_models.py
+++ src/define_models.py
@@ -3,7 +3,7 @@ import numpy as np
 import configparser
 import matplotlib.pyplot as plt
 from keras.models import Model, Sequential
-from keras.layers import Input, Convolution2D, MaxPooling2D, Dense, Dropout, Activation, Flatten, Reshape
+from keras.layers import Input, Convolution2D, Conv2D MaxPooling2D, Dense, Dropout, Activation, Flatten, Reshape
 from keras.layers.normalization import BatchNormalization
 from keras.callbacks import EarlyStopping, ModelCheckpoint, History
 from keras.utils import np_utils
