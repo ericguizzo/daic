be8b52d615d7266f21d25dc7bb9269cc5cd96208
ericguizzo <eric.guizzo@hotmail.it>
2019-08-14 16:28:40 +0100

    some changes

--- src/preprocessing_RAVDESS.py
+++ src/preprocessing_RAVDESS.py
@@ -45,7 +45,14 @@ if sr == SR:
 else:
     librosa_SR = SR
 
-def filter_data(contents, criterion, train_list, val_list, test_list):
+def extract_label_RAVDESS(input_soundfile):
+    #compute one hot label
+    label = input_soundfile.split('/')[-1].split('.')[0].split('-')[2]
+    one_hot_label = (uf.onehot(int(label)-1, num_classes_ravdess))
+
+    return one_hot_label
+
+def filter_data_RAVDESS(contents, criterion, filter_list):
     '''
     split train, val and test data accodring to criterion
     '''
@@ -55,77 +62,17 @@ def filter_data(contents, criterion, train_list, val_list, test_list):
     gender = lambda x: 1 if int(x.split('/')[-1].split('.')[0].split('-')[-1]) % 2. == 0 else 0
 
     if criterion == 'actor':
-        filtered_train = list(filter(lambda x: actor(x) in train_list, contents))
-        filtered_val = list(filter(lambda x: actor(x) in val_list, contents))
-        filtered_test = list(filter(lambda x: actor(x) in test_list, contents))
+        filtered = list(filter(lambda x: actor(x) in filter_list, contents))
     elif criterion == 'statement':
-        filtered_train = list(filter(lambda x: statement(x) in train_list, contents))
-        filtered_val = list(filter(lambda x: statement(x) in val_list, contents))
-        filtered_test = list(filter(lambda x: statement(x) in test_list, contents))
+        filtered = list(filter(lambda x: statement(x) in filter_list, contents))
     elif criterion == 'intensity':
-        filtered_train = list(filter(lambda x: intensity(x) in train_list, contents))
-        filtered_val = list(filter(lambda x: intensity(x) in val_list, contents))
-        filtered_test = list(filter(lambda x: intensity(x) in test_list, contents))
+        filtered = list(filter(lambda x: intensity(x) in filter_list, contents))
     elif criterion == 'gender':
-        filtered_train = list(filter(lambda x: gender(x) in train_list, contents))
-        filtered_val = list(filter(lambda x: gender(x) in val_list, contents))
-        filtered_test = list(filter(lambda x: gender(x) in test_list, contents))
+        filtered = list(filter(lambda x: gender(x) in filter_list, contents))
 
-    return filtered_train, filtered_val, filtered_test
+    return filtered
 
-def preprocess_datapoint(input_sound):
-    '''
-    generate predictors (stft) and target (valence sequence)
-    of one sound file from the OMG dataset
-    '''
-    raw_samples, sr = librosa.core.load(input_sound, sr=librosa_SR)  #read audio
-    if SEGMENTATION:
-        # if segment cut initial and final silence if present
-        samples = uf.strip_silence(raw_samples)
 
-    else:
-        #if not, zero pad all sounds to the same length
-        samples = np.zeros(max_file_length)
-        samples[:len(raw_samples)] = raw_samples  #zero padding
-    #samples = uf.preemphasis(samples, sr)  #apply preemphasis
-    feats = fa.extract_features(samples, FEATURES_TYPE)  #extract features
-    #compute one hot label
-    label = input_sound.split('/')[-1].split('.')[0].split('-')[2]
-    one_hot_label = (uf.onehot(int(label)-1, num_classes_ravdess))
-    return feats, one_hot_label
-
-def segment_datapoint(features, label):
-    '''
-    segment features and annotations of one long audio file
-    into smaller matrices of length "sequence_length"
-    and overlapped by "sequence_overlap"
-    '''
-    num_frames = features.shape[0]
-    step = SEQUENCE_LENGTH*SEQUENCE_OVERLAP  #segmentation overlap step
-    pointer = np.arange(0, num_frames, step, dtype='int')  #initail positions of segments
-    predictors = []
-    target = []
-    #slice arrays and append datapoints to vectors
-    if SEGMENTATION:
-        for start in pointer:
-            stop = int(start + SEQUENCE_LENGTH)
-            #print start_annotation, stop_annotation, start_features, stop_features
-            if stop <= num_frames:
-                temp_predictors = features[start:stop]
-
-                predictors.append(temp_predictors)
-                target.append(label)
-            else:  #last datapoint has a different overlap
-                temp_predictors = features[-int(SEQUENCE_LENGTH):]
-                predictors.append(temp_predictors)
-                target.append(label)
-    else:
-        predictors.append(features)
-        target.append(label)
-    predictors = np.array(predictors)
-    target = np.array(target)
-
-    return predictors, target
 
 def preprocess_dataset(sounds_list):
 
@@ -151,29 +98,14 @@ def preprocess_dataset(sounds_list):
             if str(e) == 'File format b\'FORM\'... not understood.':
                 pass
 
-        perc_progress = int((index * 100) / num_sounds)
-        index += 1
-        progress_string = "processed files: " + str(index) + " over " + str(num_sounds) + "  |  progress: " + str(perc_progress) + "%"
-        print ('\r', progress_string, end= ' ')
+        uf.print_bar(index, num_sounds)
     print ('\n')
     predictors = np.array(predictors)
     target = np.array(target)
     #predictors = np.concatenate(predictors, axis=0)  #reshape arrays
     #target = np.concatenate(target, axis=0)
 
-    #scramble datapoints order
-    shuffled_predictors = []
-    shuffled_target = []
-    num_datapoints = target.shape[0]
-    random_indices = np.arange(num_datapoints)
-    np.random.shuffle(random_indices)
-    for i in random_indices:
-        shuffled_predictors.append(predictors[i])
-        shuffled_target.append(target[i])
-    shuffled_predictors = np.array(shuffled_predictors)
-    shuffled_target = np.array(shuffled_target)
 
-    return shuffled_predictors, shuffled_target
 
 
 
