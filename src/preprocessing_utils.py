b077f649a703107f00f29186400f73bf66714d28
ericguizzo <eric.guizzo@hotmail.it>
2019-08-16 13:37:47 +0100

    adding IEMOCAP preprocessing

--- src/preprocessing_utils.py
+++ src/preprocessing_utils.py
@@ -134,8 +134,7 @@ def preprocess_datapoint(input_filename, max_file_length, librosa_SR, hop_size):
     of one sound file from the OMG dataset
     '''
     raw_samples, sr = librosa.core.load(input_filename, sr=librosa_SR)  #read audio
-    print ('culo')
-    print (sr)
+
     if SEGMENTATION:
         seq_len_samps = int(SEQUENCE_LENGTH * sr)
         #librosa does not compute last frame (zeropadding) if
@@ -145,7 +144,7 @@ def preprocess_datapoint(input_filename, max_file_length, librosa_SR, hop_size):
         pad_length = seq_len_samps + missing_samples
         pad_length = 64801
         # if segment cut initial and final silence if present
-        samples = uf.strip_silence(raw_samples)
+        #samples = uf.strip_silence(raw_samples)
         if len(samples) < pad_length:
             pad = np.zeros(pad_length)
             pad[:len(raw_samples)] = raw_samples
@@ -157,8 +156,7 @@ def preprocess_datapoint(input_filename, max_file_length, librosa_SR, hop_size):
         samples[:len(raw_samples)] = raw_samples  #zero padding
     #samples = uf.preemphasis(samples, sr)  #apply preemphasis
     feats = extract_features(samples, FEATURES_TYPE)  #extract features
-    print ('colonn')
-    print (len(samples))
+
     return feats
 
 def segment_datapoint(features, label, seq_len_frames):
