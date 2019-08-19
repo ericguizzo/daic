6af1c861a89e683f0058941b641656cf87243f92
ericguizzo <eric.guizzo@hotmail.it>
2019-08-19 13:35:07 +0100

    improved augmentation

--- src/training.py
+++ src/training.py
@@ -35,7 +35,7 @@ except IndexError:
     #generator: 11865
     #nogenerator
     generator = True
-    dataset = 'ravdess'
+    dataset = 'iemocap'
     architecture = 'OMG_model'
     parameters = ['niente = 0']
     task_type = 'classification'
