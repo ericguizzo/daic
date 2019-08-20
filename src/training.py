6623ce22c33c24bbf63f320af703c08d3fc2678a
ericguizzo <eric.guizzo@hotmail.it>
2019-08-20 16:28:57 +0100

    exps on pretraining

--- src/training.py
+++ src/training.py
@@ -450,17 +450,17 @@ def main():
     print ('')
     print ('\n train results:')
     for i in temp_results.keys():
-        if 'hist' not in i:
+        if 'hist' not in i and 'actors' not in i:
             if 'train' in i:
                 print (str(i) + ': ' + str(temp_results[i]))
     print ('\n val results:')
     for i in temp_results.keys():
-        if 'hist' not in i:
+        if 'hist' not in i and 'actors' not in i:
             if 'val' in i:
                 print (str(i) + ': ' + str(temp_results[i]))
     print ('\n test results:')
     for i in temp_results.keys():
-        if 'hist' not in i:
+        if 'hist' not in i and 'actors' not in i:
             if 'test' in i:
                 print (str(i) + ': ' + str(temp_results[i]))
 
