433ed614bf77056d18d8b28fc6dea26e6fd44fbc
ericguizzo <eric.guizzo@hotmail.it>
2019-09-19 13:47:55 +0100

    adding pytorch beckend option

--- src/training_torch.py
+++ src/training_torch.py
@@ -252,6 +252,8 @@ def main():
 
     #reshape tensors
     #INSERT HERE FUNCTION FOR CUSTOM RESHAPING!!!!!
+    print ('culo')
+    print (training_predictors.shape)
     if reshaping_type == 'cnn':
         training_predictors = training_predictors.reshape(training_predictors.shape[0], 1, training_predictors.shape[1],training_predictors.shape[2])
         validation_predictors = validation_predictors.reshape(validation_predictors.shape[0], 1, validation_predictors.shape[1], validation_predictors.shape[2])
