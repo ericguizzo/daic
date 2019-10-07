d8122525c9d0eaed1ec1b243ce0a8949194144aa
ericguizzo <eric.guizzo@hotmail.it>
2019-10-07 14:08:15 +0100

    adding tess

--- src/preprocessing_TESS.py
+++ src/preprocessing_TESS.py
@@ -52,40 +52,32 @@ def get_label_TESS(input_soundfile, num_classes=num_classes_tess):
     label = input_soundfile.split('/')[-1].split('.')[0].split('_')[-1]
     int_label = assoc_labels[label]
     one_hot_label = (uf.onehot(int(int_label)-1, num_classes_tess))
-    print (label, int_label)
 
     return one_hot_label
 
 
-contents = os.listdir(INPUT_TESS_FOLDER)
-contents = list(filter(lambda x: '.wav' in x, contents))
-contents = [os.path.join(INPUT_TESS_FOLDER, x) for x in contents]
-for i in contents:
-    get_label_TESS(i)
-print (contents)
-sys.exit(0)
-
-def filter_data_TESS(contents, criterion, item_to_filter):
+def filter_data_TESS(contents, item_to_filter):
     '''
     outputs list of soundfiles of the only actor number "item_to_filter"
     '''
-    actor = lambda x: int(x.split('/')[-1].split('.')[0].split('-')[-1])
-    statement = lambda x: int(x.split('/')[-1].split('.')[0].split('-')[-3])
-    intensity = lambda x: int(x.split('/')[-1].split('.')[0].split('-')[-4])
-    gender = lambda x: 1 if int(x.split('/')[-1].split('.')[0].split('-')[-1]) % 2. == 0 else 0
 
-    if criterion == 'actor':
-        filtered = list(filter(lambda x: actor(x) in item_to_filter, contents))
-    elif criterion == 'statement':
-        filtered = list(filter(lambda x: statement(x) in item_to_filter, contents))
-    elif criterion == 'intensity':
-        filtered = list(filter(lambda x: intensity(x) in item_to_filter, contents))
-    elif criterion == 'gender':
-        filtered = list(filter(lambda x: gender(x) in item_to_filter, contents))
+    oaf = list(filter(lambda x: 'OAF' in x, contents))
+    yaf = list(filter(lambda x: 'YAF' in x, contents))
 
-    return filtered
+    oaf_split = len(oaf) / 2
+    yaf_split = len(yaf) / 2
 
-'''
+    actors = {
+    0: oaf[:oaf_split],
+    1: oaf[oaf_split:],
+    2: yaf[:yaf_split],
+    3: yaf[yaf_split:]}
+
+    selected_list = actors[item_to_filter]
+
+    return selected_list
+
+
 def main():
 
     #custom preprocessing routine for the ravdess dataset
@@ -95,28 +87,33 @@ def main():
     print('')
     #compute max file length of current dataet
     #for the zeropadding
-    max_file_length = get_max_length_RAVDESS()
+    max_file_length = get_max_length_TESS()
     #define the list of foldable items. In the case of RAVDESS
     #actors are simply numbered from 0 to 24
-    actors_list = list(range(25))
-    contents = os.listdir(INPUT_RAVDESS_FOLDER)  #get list of filepaths
+    actors_list = list(range(4))
+    contents = os.listdir(INPUT_TESS_FOLDER)  #get list of filepaths
     contents = list(filter(lambda x: '.wav' in x, contents))  #keep only wav files
+    contents = [os.path.join(INPUT_TESS_FOLDER, x) for x in contents]
+
     #actors_list = actors_list[:2]
-    num_files = len(actors_list)
+    num_actors = len(actors_list)
     #init predictors and target dicts
     predictors = {}
     target = {}
     #create output paths for the npy matrices
     appendix = '_' + FEATURES_TYPE
-    predictors_save_path = os.path.join(OUTPUT_FOLDER, 'ravdess' + appendix + '_predictors.npy')
-    target_save_path = os.path.join(OUTPUT_FOLDER, 'ravdess' + appendix + '_target.npy')
+    predictors_save_path = os.path.join(OUTPUT_FOLDER, 'tess' + appendix + '_predictors.npy')
+    target_save_path = os.path.join(OUTPUT_FOLDER, 'tess' + appendix + '_target.npy')
     #iterate the list of actors
     index = 1  #index for progress bar
     for i in actors_list:
         #print progress bar
         #uf.print_bar(index, num_files)
         #get only soundpaths of current actor
-        curr_list = filter_data_RAVDESS(contents, 'actor', [i+1])
+        curr_list = filter_data_TESS(contents, i)
+        print ('\nCULO')
+        print (curr_list)
+        '''
         fold_string = '\nPreprocessing foldable item: ' + str(index) + '/' + str(num_files)
         print (fold_string)
         #make sure that each item list is a FULL path to a sound file
@@ -128,6 +125,7 @@ def main():
         #append preprocessed predictors and target to the dict
         predictors[i] = curr_predictors
         target[i] = curr_target
+        '''
         index +=1
     #save dicts
     #save dicts
@@ -152,4 +150,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-'''
