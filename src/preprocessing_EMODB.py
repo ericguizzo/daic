3db8f2e93f4aeb6c1d25207f0c58589e4a3c72d1
ericguizzo <eric.guizzo@hotmail.it>
2019-10-07 15:05:25 +0100

    adding emodb

--- src/preprocessing_EMODB.py
+++ src/preprocessing_EMODB.py
@@ -102,22 +102,19 @@ def main():
         #uf.print_bar(index, num_files)
         #get only soundpaths of current actor
         curr_list = filter_data_EMODB(contents, i)
-        print ('')
-        print ('culo')
-        print (curr_list[:3])
-        '''
+
+
         fold_string = '\nPreprocessing foldable item: ' + str(index) + '/' + str(num_files)
         print (fold_string)
         #make sure that each item list is a FULL path to a sound file
         #and not only the sound name as os.listdir outputs
-        curr_list = [os.path.join(INPUT_RAVDESS_FOLDER, x) for x in curr_list]
         #preprocess all sounds of the current actor
         #args:1. listof soundpaths of current actor, 2. max file length, 3. function to extract label from filepath
-        curr_predictors, curr_target = pre.preprocess_foldable_item(curr_list, max_file_length, get_label_RAVDESS, True)
+        curr_predictors, curr_target = pre.preprocess_foldable_item(curr_list, max_file_length, get_label_EMODB, True)
         #append preprocessed predictors and target to the dict
         predictors[i] = curr_predictors
         target[i] = curr_target
-        '''
+
         index +=1
     #save dicts
     #save dicts
